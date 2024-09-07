076220ed95e2d315bf8b8fc0ddfb0dca87c5d219
Eses201 <eses50396@gmail.com>
2024-09-07 11:03:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showDancingMonkey() {
+    // Change the image source to Dancing Monkey GIF
+    document.getElementById("monkey-gif").innerHTML = '<img src="https://media.tenor.com/6-KkdA08LgYAAAAC/dancing-monkey.gif" alt="Dancing Monkey">';
+}
